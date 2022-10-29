2142e34f60af356e8331fdd3f6a3f9b0507dd270
Ragul <ragulece1999@gmail.com>
2022-10-29 08:40:45 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { Companant1Component } from './component1/companant1.component';
-import { homeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   {
-    path:'home',component:homeComponent
+    path:'home',component:Companant1Component
   },
-  {
-    path:'about-us',component:AboutUsComponent
-  },
-  
 ];
 
 @NgModule({
